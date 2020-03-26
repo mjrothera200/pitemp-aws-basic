bb1a6521098c7a0664707d0b6d4ad809d6c5bb8b
mjrothera200 <mjrothera200@yahoo.com>
2020-03-26 17:03:30 -0400

    Got it working!

--- iot-temp.py
+++ iot-temp.py
@@ -5,22 +5,11 @@ import dht11
 import time
 import datetime
 import ssl
-import logging, traceback
 
-IoT_protocol_name = "x-amzn-mqtt-ca"
-aws_iot_endpoint = "AWS_IoT_ENDPOINT_HERE" # <random>.iot.<region>.amazonaws.com
-url = "https://{}".format(aws_iot_endpoint)
 
-ca = "YOUR/ROOT/CA/PATH" 
-cert = "YOUR/DEVICE/CERT/PATH"
-private = "YOUR/DEVICE/KEY/PATH"
-
-logger = logging.getLogger()
-logger.setLevel(logging.DEBUG)
-handler = logging.StreamHandler(sys.stdout)
-log_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-handler.setFormatter(log_format)
-logger.addHandler(handler)
+ca = "/home/pi/dev/iot-temp/pitemp-aws-basic/AmazonRootCA1.pem" 
+cert = "/home/pi/dev/iot-temp/pitemp-aws-basic/bf1cf3d223-certificate.pem.crt"
+private = "/home/pi/dev/iot-temp/pitemp-aws-basic/bf1cf3d223-private.pem.key"
 
 
 
@@ -44,6 +33,10 @@ except:
 
 print("CPU Serial # is %s" % (cpuserial))
 
+#aws_iot_endpoint = cpuserial+".iot.us-east-1.amazonaws.com" # <random>.iot.<region>.amazonaws.com
+aws_iot_endpoint = "data.iot.us-east-1.amazonaws.com" # <random>.iot.<region>.amazonaws.com
+IoT_protocol_name = "x-amzn-mqtt-ca"
+url = "https://{}".format(aws_iot_endpoint)
 
 # --------------- #
 # Callback events #
@@ -72,10 +65,13 @@ def on_message(client, obj, msg):
 def ssl_alpn():
     try:
         #debug print opnessl version
-        logger.info("open ssl version:{}".format(ssl.OPENSSL_VERSION))
+        print("open ssl version:{}".format(ssl.OPENSSL_VERSION))
         ssl_context = ssl.create_default_context()
-        ssl_context.set_alpn_protocols([IoT_protocol_name])
+        #ssl_context.set_alpn_protocols([IoT_protocol_name])
+        print("ca:{}".format(ca))
         ssl_context.load_verify_locations(cafile=ca)
+        print("cert:{}".format(cert))
+        print("private:{}".format(private))
         ssl_context.load_cert_chain(certfile=cert, keyfile=private)
 
         return  ssl_context
@@ -84,39 +80,37 @@ def ssl_alpn():
         raise e
 
 if __name__ == '__main__':
-    topic = "test/date"
+    out_topic_environmentals = "environmentals"
     try:
-        mqttc = mqtt.Client()
+        client = paho.Client()
         client.on_message = on_message
         client.on_connect = on_connect
         client.on_disconnect = on_disconnect
         client.on_subscribe = on_subscribe
         ssl_context= ssl_alpn()
-        mqttc.tls_set_context(context=ssl_context)
-        logger.info("start connect")
-        mqttc.connect(aws_iot_endpoint, port=443)
-        logger.info("connect success")
-        mqttc.loop_start()
+        client.tls_set_context(context=ssl_context)
+        print("start connect: {}".format(aws_iot_endpoint))
+        client.connect(aws_iot_endpoint, port=8883)
+        print("connect success")
+        client.loop_start()
 
         while True:
             now = datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
-            logger.info("try to publish:{}".format(now))
-            mqttc.publish(topic, now)
+            print("try to publish:{}".format(now))
+            result = instance.read()
+            if result.is_valid():
+                temp_c = result.temperature 
+                temp_f = (result.temperature * 9)/5 + 32 
+                humidity = result.humidity 
+                message = { 'd': {'temp_c': temp_c, 'temp_f': temp_f, 'humidity': humidity } }     
+                client.publish(out_topic_environmentals, json.dumps(message))
             time.sleep(1)
 
     except Exception as e:
-        logger.error("exception main()")
-        logger.error("e obj:{}".format(vars(e)))
-        logger.error("message:{}".format(e.message))
-        traceback.print_exc(file=sys.stdout)
+        print("exception main()")
+        print("e obj:{}".format(vars(e)))
+        print("message:{}".format(e.message))
 
-    # result = instance.read()
-    # if result.is_valid():
-    #	temp_c = result.temperature 
-    #	temp_f = (result.temperature * 9)/5 + 32 
-    #	humidity = result.humidity 
-    #	message = { 'd': {'temp_c': temp_c, 'temp_f': temp_f, 'humidity': humidity } }     
-    #	client.publish(out_topic_environmentals, json.dumps(message))
 
 
 
